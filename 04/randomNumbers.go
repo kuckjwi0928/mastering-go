1a51bab1a39c25f1c7636d2108c8398f3d602be4
kuckjwi <rnrghks09@gmail.com>
2021-10-24 19:57:43 +0900

    add random numbers go

---
+++ 04/randomNumbers.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	MIN := 0
+	MAX := 100
+	TOTAL := 100
+	SEED := time.Now().Unix()
+
+	rand.Seed(SEED)
+
+	for i := 0; i < TOTAL; i++ {
+		myrand := random(MIN, MAX)
+		fmt.Print(myrand, " ")
+	}
+
+	fmt.Println()
+}
+
+func random(min, max int) int {
+	return rand.Intn(max-min) + min
+}

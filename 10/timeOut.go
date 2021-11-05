940f6e802ce0a4f636738b2722ad96b9adf62d72
kuckjwi <rnrghks09@gmail.com>
2021-11-05 16:53:09 +0900

    modified channel timeout example

--- 10/timeOut.go
+++ 10/timeOut.go
@@ -7,29 +7,32 @@ import (
 
 func main() {
 	c1 := make(chan string)
+	c2 := make(chan string)
+
+	fmt.Println(time.Now())
 
 	go func() {
-		time.Sleep(time.Second * 3)
+		time.Sleep(4 * time.Second)
 		c1 <- "c1 OK!"
 	}()
 
-	select {
-	case res := <-c1:
-		fmt.Println(res)
-	case <-time.After(1 * time.Second):
-		fmt.Println("timeout c1")
-	}
-
-	c2 := make(chan string)
 	go func() {
-		time.Sleep(3 * time.Second)
-		c2 <- "c2 OK"
+		time.Sleep(1 * time.Second)
+		c2 <- "c2 OK!"
 	}()
 
-	select {
-	case res := <-c2:
-		fmt.Println(res)
-	case <-time.After(4 * time.Second):
-		fmt.Println("timeout c2")
+	for {
+		select {
+		case res := <-c1:
+			fmt.Println(time.Now())
+			fmt.Println(res)
+		case res := <-c2:
+			fmt.Println(time.Now())
+			fmt.Println(res)
+		case <-time.After(2 * time.Second):
+			fmt.Println(time.Now())
+			fmt.Println("timeout")
+			return
+		}
 	}
 }
